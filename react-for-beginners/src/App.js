272176ed6a9f1ac15d561b23f8667e6f63f5800a
Yeongkyeong12 <ohkyeong1995@gmail.com>
2021-12-14 15:26:18 +0900

    practiced useEffect

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,7 +1,25 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+
   return (
     <div>
-      <h1>Welcome back!</h1>
+      <input onChange={onChange} value={keyword} placeholder="Search here..." />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
